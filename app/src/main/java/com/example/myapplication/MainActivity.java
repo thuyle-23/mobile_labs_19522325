9b6ab9645ad0eb4a983f2605af14401eda6236da
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-03-28 23:09:43 +0700

    grid

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +19,13 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+
+    GridView gridView;
+    String[] name = {"He'd have you all unravel at the",
+                    "Heed not the rabble",
+                    "Sound of screams but the",
+                    "Who scream"};
+    int[] colorname = {R.color.color8, R.color.color9, R.color.color10, R.color.color11};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +38,20 @@ public class MainActivity extends AppCompatActivity {
         String[] itemList = new String[]{
                 "", "", ""
         };
-        String[] strView = new String[]{"He'd have you all unravel at the",
-                "Heed not the rabble",
-                "Sound of screams but the",
-                "Who scream"};
+
+        gridView = findViewById(R.id.gridview);
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Intent intent = new Intent(getApplicationContext(), GridActivity.class);
+                intent.putExtra("name", name[position]);
+                intent.putExtra("image", colorname[position]);
+                startActivity(intent);
+            }
+        });
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, itemList) {
             @NonNull
@@ -49,5 +72,33 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         listView.setAdapter(arrayAdapter);
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return colorname.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.grid_list, null);
+            TextView name1 = view.findViewById(R.id.text);
+            ImageView image = view.findViewById(R.id.image);
+            name1.setText(name[position]);
+            image.setImageResource(colorname[position]);
+            return view;
+        }
     }
 }
