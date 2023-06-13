1cac8417337d8bc0673a940f28a4c839a2f77cc8
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-06-13 07:38:10 +0700

    test

--- app/src/main/java/com/example/mobile_labs_19522325/MainActivity.java
+++ app/src/main/java/com/example/mobile_labs_19522325/MainActivity.java
@@ -8,15 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -25,66 +26,62 @@ import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button btnAdd, btnQueryAll;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private Button btnFadeInXml, btnFadeInCode, btnFadeOutXml, btnFadeOutCode, btnBlinkXml,
+            btnBlinkCode, btnZoomInXml, btnZoomInCode, btnZoomOutXml, btnZoomOutCode, btnRotateXml,
+            btnRotateCode, btnMoveXml, btnMoveCode, btnSlideUpXml, btnSlideUpCode, btnBounceXml,
+            btnBounceCode, btnCombineXml, btnCombineCode;
+    private ImageView ivUitLogo;
+    private Animation.AnimationListener animationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnQueryAll = (Button) findViewById(R.id.btnQueryAll);
-
-        btnAdd.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Map<String, Object> users = new HashMap<>();
-                users.put("fullName", "Nguyen Van D");
-                users.put("phoneNumber", "0986372617");
-
-                db.collection("users")
-                        .add(users)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
-                            }
-                        });
-            }
-        });
-
-//        public void fetchData(){
-//            DocumentReference document = db.collection("student").document("row3");
-//            document.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>(){
-//
-//            });
-//        }
-        btnQueryAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Map<String, Object> users = new HashMap<>();
-
-                db.collection("users")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        Log.d(TAG, document.getId() + " => " + document.getData());
-                                    }
-                                } else {
-                                    Log.w(TAG, "Error getting documents.", task.getException());
-                                }
-                            }
-                        });
-            }
-        });
     }
+    private void findViewsByIds() {
+        ivUitLogo = (ImageView) findViewById(R.id.iv_uit_logo);
+        btnFadeInXml = (Button) findViewById(R.id.btn_fade_in_xml);
+        btnFadeInCode = (Button) findViewById(R.id.btn_fade_in_code);
+        btnFadeOutXml = (Button) findViewById(R.id.btn_fade_out_xml);
+        btnFadeOutCode = (Button) findViewById(R.id.btn_fade_out_code);
+        btnBlinkXml = (Button) findViewById(R.id.btn_blink_xml);
+        btnBlinkCode = (Button) findViewById(R.id.btn_blink_code);
+        btnZoomInXml = (Button) findViewById(R.id.btn_zoom_in_xml);
+        btnZoomInCode = (Button) findViewById(R.id.btn_zoom_in_code);
+        btnZoomOutXml = (Button) findViewById(R.id.btn_zoom_out_xml);
+        btnZoomOutCode = (Button) findViewById(R.id.btn_zoom_out_code);
+        btnRotateXml = (Button) findViewById(R.id.btn_rotate_xml);
+        btnRotateCode = (Button) findViewById(R.id.btn_rotate_code);
+        btnMoveXml = (Button) findViewById(R.id.btn_move_xml);
+        btnMoveCode = (Button) findViewById(R.id.btn_move_code);
+        btnSlideUpXml = (Button) findViewById(R.id.btn_slide_up_xml);
+        btnSlideUpCode = (Button) findViewById(R.id.btn_slide_up_code);
+        btnBounceXml = (Button) findViewById(R.id.btn_bounce_xml);
+        btnBounceCode = (Button) findViewById(R.id.btn_bounce_code);
+        btnCombineXml = (Button) findViewById(R.id.btn_combine_xml);
+        btnCombineCode = (Button) findViewById(R.id.btn_combine_code);
+    }
+    private void initVariables() {
+        animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Toast.makeText(getApplicationContext(), "Animation Stopped",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+    }
+
 }
